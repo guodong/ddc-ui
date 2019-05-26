baf832ce08385fee2b2a9edeca0711e0d5f0d330
guodong <gd@tongji.edu.cn>
2019-05-26 13:07:06 +0800

    Updates

---
+++ precache-manifest.087714eeec21173c431d1c5391dc300b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c83a6f50d2c0ba474b82cb31c64b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b81e643689daf604907",
+    "url": "/static/css/2.bdd1d7b3.chunk.css"
+  },
+  {
+    "revision": "dc7e8588acf94461a5aa",
+    "url": "/static/css/main.8c9b7b49.chunk.css"
+  },
+  {
+    "revision": "0b81e643689daf604907",
+    "url": "/static/js/2.06d7554e.chunk.js"
+  },
+  {
+    "revision": "dc7e8588acf94461a5aa",
+    "url": "/static/js/main.92791436.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
