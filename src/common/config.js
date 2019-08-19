ea80e73cc52aa2c4f1a693ff0288ecce9f52cc76
LHBrave <onfnju@163.com>
2019-08-19 12:06:00 +0800

    change API

--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,6 @@
-const API = "http://api.ddc.kfcoding.com";
+// const API = "http://api.ddc.kfcoding.com";
 // const API = 'http://192.168.2.5:9000';
+ const API = 'http://127.0.0.1:3000';
+
 export default API;
 
